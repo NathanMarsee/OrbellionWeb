d171c0f52246d5ee786310b56066fa3c9baa5367
Nathan Marsee <natemarsee@icloud.com>
2026-02-28 19:22:31 -0800

    Implement simple drag & drop

---
+++ OrbellionWeb/wwwroot/JSFolder/DragAndDrop.js
@@ -0,0 +1,26 @@
+﻿function dragAndDrop(className) {
+    interact(className).draggable({
+        listeners: {
+            start(event) {
+                // Initialize per-element position if missing
+                if (!event.target.dataset.x) event.target.dataset.x = 0;
+                if (!event.target.dataset.y) event.target.dataset.y = 0;
+            },
+            move(event) {
+                let x = (parseFloat(event.target.dataset.x) || 0) + event.dx;
+                let y = (parseFloat(event.target.dataset.y) || 0) + event.dy;
+
+                event.target.style.transform = `translate(${x}px, ${y}px)`;
+
+                event.target.dataset.x = x;
+                event.target.dataset.y = y;
+            }
+        },
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: '.main',
+                endOnly: true
+            })
+        ]
+    });
+}
