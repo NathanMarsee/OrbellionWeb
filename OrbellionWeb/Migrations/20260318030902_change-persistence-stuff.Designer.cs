56b1e8ab41948cfbf8f3b1a330a8514bb8e672ad
hbarton2 <hbarton1337@gmail.com>
2026-03-17 20:28:26 -0700

    Added persistence for Staples and Hellions, Added hellion visuals to user Home page. Also fixed crash when moving cards from hand to battlefield.

--- OrbellionWeb/Migrations/20260318030902_change-persistence-stuff.Designer.cs
+++ OrbellionWeb/Migrations/20260318030902_change-persistence-stuff.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrbellionWeb.Data;
 
@@ -10,9 +11,11 @@ using OrbellionWeb.Data;
 namespace OrbellionWeb.Migrations
 {
     [DbContext(typeof(OrbellionWebContext))]
-    partial class OrbellionWebContextModelSnapshot : ModelSnapshot
+    [Migration("20260318030902_change-persistence-stuff")]
+    partial class changepersistencestuff
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
