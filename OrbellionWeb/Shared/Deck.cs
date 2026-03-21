e49add588e8958a3425ea6e12543a57e1684755a
hbarton2 <hbarton1337@gmail.com>
2026-03-21 13:46:25 -0700

    Added simple deck functionality (still need the dragging stuff)

---
+++ OrbellionWeb/Shared/Deck.cs
@@ -0,0 +1,54 @@
+﻿using OrbellionWeb.Models;
+using System.Drawing.Configuration;
+
+namespace OrbellionWeb.Shared
+{
+    public class Deck
+    {
+        public IEnumerable<Card> Cards { get; set; }
+
+        public Deck() 
+        { 
+            Cards = new List<Card>();
+        }
+        public Deck(IEnumerable<Card> cards)
+        {
+            Cards = cards;
+        }
+
+        public Card? Draw()
+        {
+            Card? result = null;
+
+            if (Cards.Any())
+            {
+                var card = Cards.First();
+                Cards = Cards.Skip(1);
+                result = card;
+            }
+
+            return result;
+        }
+
+        public void Shuffle()
+        {
+            var rnd = new Random();
+            Cards = Cards.OrderBy(x => rnd.Next());
+        }
+
+        public void PutOnTop(Card card)
+        {
+            Cards = new[] { card }.Concat(Cards);
+        }
+
+        public void PutOnBottom(Card card)
+        {
+            Cards = Cards.Concat([card]);
+        }
+
+        public Card? Tutor(Guid id)
+        {
+            return Cards.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
